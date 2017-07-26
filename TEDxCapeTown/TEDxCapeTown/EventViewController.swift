3b606ed07c063fdbff5a90d50042049b2c85d9b0
Carla <carla.goldstein@gmail.com>
2017-07-26 20:28:52 +0100

    added basic icons and tab controllers

---
+++ TEDxCapeTown/TEDxCapeTown/EventViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EventViewController.swift
+//  TEDxCapeTown
+//
+//  Created by Carla on 2017/07/26.
+//  Copyright © 2017 TEDxCapeTown. All rights reserved.
+//
+
+import Foundation
---
+++ TEDxCapeTown/TEDxCapeTown/TalksViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TEDxCapeTown
+//
+//  Created by Carla on 2017/07/26.
+//  Copyright © 2017 TEDxCapeTown. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
